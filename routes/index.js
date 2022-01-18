9854b8407bdcdf2ce16b3b33aad1e2c9238c6846
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:55:11 -0500

    fixed error in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const htmlRoutes = require("./htmlRoutes");
 const apiRoutes = require("./apiRoutes");
 const router = require("express").Router();
 
-router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
+router.use("/", htmlRoutes);
 
 module.exports = router;
