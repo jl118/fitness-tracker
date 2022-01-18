be2ad250e25f23002ba629d832e4a1fb446ed669
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:53:56 -0500

    worked on server.js

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(logger("dev"));
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// TODO: add logger and connection
+// TODO: add connection
 
 app.use(routes);
 
