5c50f4e54e7765f646528d31fc38cf85983a811f
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:30:12 -0500

    worked on update existing workout route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,18 @@ const app = require("express").Router();
 const { Workout } = require("../models");
 const db = require("../models");
 
+// latest workout
 app.get("/workouts", (req, res) => {
     db.Workout.find({})
-        .then((dbWorkouts) => {
-            res.json(dbWorkouts);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+    .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
 });
 
+// past 7 workouts 
 app.get("/workouts/range", (req, res) => {
     db.Workout.aggregate([
         {
@@ -30,10 +32,24 @@ app.get("/workouts/range", (req, res) => {
     });
 });
 
+// update existing workout
 app.put("/workouts/:id", (req, res) => {
-  
+    db.Workout.findByIdAndUpdate(
+        req.params.id ,
+        { $push: {exercises: req.body}  },
+        { new: true, runValidators: true }
+    )
+    .then((dbWorkout) => {
+        console.log(dbWorkout)
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        console.log(err)
+        res.json(err);
+    });
 });
 
+// TODO: add a new workout
 app.post("/workouts", (req, res) => {
   
 });
