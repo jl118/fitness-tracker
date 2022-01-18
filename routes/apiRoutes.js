d66dd8d431c447aeb835c8efb45eef52c9f1f320
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:21:38 -0500

    worked on get route for past 7 workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,7 +13,21 @@ app.get("/workouts", (req, res) => {
 });
 
 app.get("/workouts/range", (req, res) => {
-  
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+            },
+        },
+    ])
+    .sort({ day: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
 });
 
 app.put("/workouts/:id", (req, res) => {
