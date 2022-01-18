009b4b2e9d645fe512d8fdabb7d21c68bca2c5a6
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:34:29 -0500

    worked on html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const db = require("../models");
+const path = require("path");
+
+// TODO: add index route
+
+// TODO: add stats route
+
+// TODO: add exercise route
+
+module.exports = router;
