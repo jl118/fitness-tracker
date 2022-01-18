198dbb84e678223fa6acf221be56344a20be1a6e
jl118 <jen.liebelt@gmail.com>
2022-01-18 16:17:02 -0500

    worked on api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const app = require("express").Router();
+const { Workout } = require("../models");
+const db = require("../models");
+
+app.get("/workouts", (req, res) => {
+
+});
+
+app.get("/workouts/range", (req, res) => {
+  
+});
+
+app.put("/workouts/:id", (req, res) => {
+  
+});
+
+app.post("/workouts", (req, res) => {
+  
+});
+
+module.exports = app;
